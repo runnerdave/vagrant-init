c716a78669e594f097c450271a3480c207608f83
David Jimenez <david.jimenez@rmit.edu.au>
2016-12-06 14:49:17 +1100

    openned firewall port for mysql

--- scripts/centos-database.sh
+++ scripts/centos-database.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # MySQL - maria db
-#does not work in centos yum install -y mysql mysql-server mysql-devel
+#does not work in centos 7 yum install -y mysql mysql-server mysql-devel
 yum install -y mysql mariadb-server
 systemctl enable mariadb
 systemctl start mariadb
+
+firewall-cmd --permanent --add-port=3306/tcp
+firewall-cmd --reload
 
 mysql -u root -e "SHOW DATABASES";
